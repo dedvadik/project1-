017d741d8252e056f5d96ea6e44776af83858554
dedvadik <desvadik@outlook.com>
2025-03-25 16:07:13 +0300

    задача про черепаху и зайца, оптимизированная

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-first_animal = "Заяц"
-second_animal = "Черепаха"
-sleep = input("Что делает заяц? ")
-run = input("Что делает черепаха? ")
+first_animal, second_animal, sleep, run = "заяц", "черепаха", input("Что делает заяц? "), input("Что делает черепаха? ")
 print(first_animal, sleep, second_animal, run)
