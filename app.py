458b53e4236c4e027ec6407e0910b55d84e8ff71
dedvadik <desvadik@outlook.com>
2025-03-25 16:12:03 +0300

    Задача про романа и прова доступа Что оценивается 	Результат вывода соответствует картинке. 	Переменные имеют значащие имена, не только a, b, c, d (видео 2.3). 	Нет смешения символов из разных алфавитов в именах переменных. 	Пробелы после запятых, пробелы при бинарных операциях. 	После запятой стоит пробел, перед запятой пробела нет. 	Отсутствуют пробелы после print и перед скобками: print () — неверно, print() — верно. 	Нет пробелов и внутри скобок.

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-first_animal, second_animal, sleep, run = "заяц", "черепаха", input("Что делает заяц? "), input("Что делает черепаха? ")
-print(first_animal, sleep, second_animal, run)
+first_name = input('Введите имя пользователя: ')
+greeting = 'Утро доброе'
+print(greeting, first_name)
+info1 = "К сожалению, у Вас нет доступа к системе."
+info2 = "Пожалуйста, обратитесь к системному администратору."
+print(info1)
+print(info2)
