cf63dd3e617005189422a9fb6f471132239a33ef
dedvadik <desvadik@outlook.com>
2025-03-25 16:17:42 +0300

    Вывод уведомления про вылет и прилёт Что оценивается 	Input содержит корректное приглашение для ввода: input() — неверно. 	Результат вывода соответствует заданию. 	Переменные имеют значащие имена, не только a, b, c, d (видео 2.3). 	Пробелы после запятых, пробелы при бинарных операциях (конкатенация в данной задаче). 	После запятой стоит пробел, перед запятой пробела нет. 	Отсутствуют пробелы после print и перед скобками: print () — неверно, print() — верно. 	Нет пробелов и внутри скобок.

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-first_name = input('Введите имя пользователя: ')
-greeting = 'Утро доброе'
-print(greeting, first_name)
-info1 = "К сожалению, у Вас нет доступа к системе."
-info2 = "Пожалуйста, обратитесь к системному администратору."
-print(info1)
-print(info2)
+city_run = input("Укажите город вылета: ")
+city_seccond = input("Укажите город прилёта: ")
+print(city_run, "-", city_seccond)
