8714900fc7af34ee5776be89f8cd2ac0872acd79
dedvadik <desvadik@outlook.com>
2025-03-25 17:53:45 +0300

    3.2.3 Проверка деления на 0 Ошибка как и должна быть ZeroDivisionError: division by zero

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-first, seccond, third = 10, 12, 28
-print("Сложение:", first + seccond + third)
-print("Вычитание:", first - seccond - third)
-print("Умножение:", first * seccond * third)
-print("Деление:", first / seccond / third)
-print("Возведение в степень:", first ** seccond ** third)
+print(143 / 0)
