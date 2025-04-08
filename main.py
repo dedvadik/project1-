dfa01b3e3a94a990f2217d1c8d677d8b090c077b
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-04-08 13:15:22 +0300

    улучшеный код

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-number1 = int(input("Введите первое число:"))
-number2 = int(input("Введите второе число:"))
-number3 = int(input("Введите третье число:"))
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+number3 = int(input("Введите третье число: "))
 
 def result():
     if number1 >= number2 and number2 >= number3:
-        print(number1)
+        result1 = number1
     elif number2 >= number1 and number2 >= number3:
-        print(number2)
+        result1 = number2
     else:
-        print(number3)
+        result1 = number3
+    
+    print("Наибольшее число:", result1)
 
-result() 
+result()
