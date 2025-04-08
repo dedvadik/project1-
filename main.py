1c75fff7e274344df2c05ba1b7fe71b7cf8fe8e7
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-04-08 12:47:17 +0300

    4.11 Задача 8 из документа Пользователь вводит три числа.  Напишите программу, которая выводит на экран максимальное из этих трёх чисел (все числа разные). Используйте дополнительные переменные, если нужно.
    Что оценивается
    результат вычислений корректен;
    input содержит корректное приглашение для ввода;
    правильное употребление пробелов после запятых и при бинарных операциях.
    
    Что оценивается во всей практической работе
    избегание пустых или неинформативных input (видео 2.3);
    избегание неинформативных имён переменных;
    избегание использования в качестве имён встроенных функций (список встроенных функций — официальная документация);
    правильное употребление пробелов после запятых и при бинарных операциях.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-hours =int(input("Введите отработанные часы: ")) 
-credit = int(input("Введите остаток по кредиту: "))
-foods = int(input("Введите траты на еду: "))
+number1 = int(input("Введите первое число:"))
+number2 = int(input("Введите второе число:"))
+number3 = int(input("Введите третье число:"))
 
-if ((200 * hours) / (2 ** 3)) + hours < credit + foods:
-    print("Часов не хватает. Придётся работать больше!")
-else:
-    print("Часов хватает. Можно отдохнуть")
+def result():
+    if number1 >= number2 and number2 >= number3:
+        print(number1)
+    elif number2 >= number1 and number2 >= number3:
+        print(number2)
+    else:
+        print(number3)
+
+result()
