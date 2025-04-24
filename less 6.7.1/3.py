e02616d37c8aa8f01adb0e62c13932404923ffb0
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-04-24 14:30:37 +0300

    Задача 3 выполнена

--- less 6.7.1/3.py
+++ less 6.7.1/3.py
@@ -1,5 +1,11 @@
 n = int(input("Ведите число которое вам принесли: "))
-number = 1
+count = 0
 
-while number <= n:
+if n == 0:
+    print("Количество цифр: 1")
+else:
+    while n > 0:
+        count += 1
+        n = n // 10
     
+    print(f"Количество цифр: {count}")
