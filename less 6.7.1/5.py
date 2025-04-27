1213d44fee05d46d20780300b4bcb89c1e90f437
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-04-27 12:27:47 +0300

    5 задача выполнена, файлы для 6 и 7 задания сделаны.

--- less 6.7.1/5.py
+++ less 6.7.1/5.py
@@ -2,18 +2,15 @@ hours = 1
 ticket = 0
 
 print("Начался восьмичасовой рабочий день.")
-print(f"{hours}-й час")
 
-while hours != 9:
+while hours <= 8:
+    print(f"{hours}-й час")
     ticket += int(input("Сколько задач решит Максим? "))
     colling = int(input("Звонит жена. Взять трубку? (1 - да, 0 - нет): "))
     hours += 1
-    if hours <= 8:
-        print(f"{hours}-й час")
-    else:
-        break
-    if colling == 1:
-        colling = "Нужно зайти в магазин."
-
+    
 print(f"Рабочий день закончился. Всего выполнено задач: {ticket}")
-print(colling)
+
+if colling == 1:
+    colling = "Нужно зайти в магазин."
+    print(colling)
