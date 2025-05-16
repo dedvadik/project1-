8a2e39e8ed266604212e20a72f26cc92adfb7a1d
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-05-16 23:06:59 +0300

    modified:   lessons 8/p2.3.py

--- lessons 8/p2.3.py
+++ lessons 8/p2.3.py
@@ -1,7 +1,9 @@
-wakeUp = int('What time ypu wake up?')
+wakeUp = int(input('What time ypu wake up? '))
 water = 0
 callSum = 0 
-
 for hour in range(wakeUp, 23 + 1, 3):
     water += 1
-    call = int(input('How many you eat callories?'))
+    print(f'{hour} o`clock.')
+    call = int(input('How many you eat callories? '))
+    callSum += call
+print(f'Sasha drink {water} liter water and eat {callSum} callories in a all day.')
