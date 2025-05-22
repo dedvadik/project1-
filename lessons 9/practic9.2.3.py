570eeb999ac56f842fb54727c33469a11bae605d
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-05-22 17:26:23 +0300

    modified:   lessons 9/practic9.2.3.py 	new file:   lessons 9/practic9.4.1.py 	new file:   lessons 9/practic9.4.2.py 	new file:   lessons 9/practic9.4.3.py

--- lessons 9/practic9.2.3.py
+++ lessons 9/practic9.2.3.py
@@ -1,21 +1,24 @@
 answerAll = ''
 count = 0
+
 name = input('Как тебя зовут? ')
 answer = input(f'{name}, Купи слона! ')
 
 while True:
+
     if answer == 'нет':
         print(f'Ладно {name}, тогда не тревожу.')
         break
+
+    elif answer == '':
+        answer = input(f'{name}, Купи слона! ')
+
     elif count > 0:
-        answerAll += ', '
-        answerAll += answer
+        answerAll += ', ' + answer
         count += 1
         answer = input(f'Все говорят {answerAll}, а ты купи слона! ')
-        continue
+
     else:
         answerAll += answer
         count += 1
         answer = input(f'Все говорят {answerAll}, а ты купи слона! ')
-        continue
-    
