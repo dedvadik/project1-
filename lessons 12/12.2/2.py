c56b9071c67f267c066549a7f636345bfa6d422d
dedvadik <77642225+dedvadik@users.noreply.github.com>
2025-06-23 17:22:40 +0300

    new file:   lessons 12/12.2/1.py new file:   lessons 12/12.2/2.py new file:   lessons 12/12.2/3.py new file:   lessons 12/12.3/1.py

---
+++ lessons 12/12.2/2.py
@@ -0,0 +1,11 @@
+def generalCode():
+    a = int(input())
+    b = int(input())
+    print("Всего", a+b, "шт.")
+
+print("Сколько мешков рыбы и мяса?")
+generalCode()
+print("Сколько буханок белого и чёрного хлеба?")
+generalCode()
+print("Сколько вёдер воды и молока?")
+generalCode()
---
+++ lessons 12/12.2/3.py
@@ -0,0 +1,8 @@
+def info():
+    print('Last Name: Dobridnev')
+    print('Fist Name: Vadim')
+    print('Street: ', end = '\n')
+
+info()
+info()
+info()
